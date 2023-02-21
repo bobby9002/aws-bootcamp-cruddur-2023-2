57c2ec964499abc7f5c1a6f1aa4b25286869c1bc
bobby9002 <104903728+bobby9002@users.noreply.github.com>
2023-02-21 18:29:06 +0000

    updated

--- backend-flask/backend-flask/app.py
+++ backend-flask/backend-flask/app.py
@@ -1,61 +1,93 @@
+import os
+import sys
 from flask import Flask, request
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 
-from services.home_activities import HomeActivities
-from services.user_activities import UserActivities
-from services.create_activity import CreateActivity
-from services.create_reply import CreateReply
-from services.search_activities import SearchActivities
-from services.message_groups import MessageGroups
-from services.messages import Messages
-from services.create_message import CreateMessage
-from services.show_activity import ShowActivity
+# Add the main application directory to the Python path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
+# Use relative imports to import modules from the services subdirectory
+from services.home_activities import *
+from services.user_activities import *
+from services.create_activity import *
+from services.create_reply import *
+from services.search_activities import *
+from services.message_groups import *
+from services.messages import *
+from services.create_message import *
+from services.show_activity import *
 
 app = Flask(__name__)
-
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
-
-cors = CORS(app, resources={
-    r"/api/*": {
-        "origins": origins
-    }
-}, expose_headers="location,link", allow_headers="content-type,if-modified-since", methods="OPTIONS,GET,HEAD,POST")
-
+cors = CORS(
+  app, 
+  resources={r"/api/*": {"origins": origins}},
+  expose_headers="location,link",
+  allow_headers="content-type,if-modified-since",
+  methods="OPTIONS,GET,HEAD,POST"
+)
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
-    user_handle = 'andrewbrown'
-    model = MessageGroups.run(user_handle=user_handle)
-    if model['errors'] is not None:
-        return model['errors'], 422
-    else:
-        return model['data'], 200
-
+  user_handle  = 'andrewbrown'
+  model = MessageGroups.run(user_handle=user_handle)
+  if model['errors'] is not None:
+    return model['errors'], 422
+  else:
+    return model['data'], 200
 
 @app.route("/api/messages/@<string:handle>", methods=['GET'])
 def data_messages(handle):
-    user_sender_handle = 'andrewbrown'
-    user_receiver_handle = request.args.get('user_receiver_handle')
+  user_sender_handle = 'andrewbrown'
+  user_receiver_handle = request.args.get('user_reciever_handle')
 
-    model = Messages.run(user_sender_handle=user_sender_handle, user_receiver_handle=user_receiver_handle)
-    if model['errors'] is not None:
-        return model['errors'], 422
-    else:
-        return model['data'], 200
+  model = Messages.run(user_sender_handle=user_sender_handle, user_receiver_handle=user_receiver_handle)
+  if model['errors'] is not None:
+    return model['errors'], 422
+  else:
+    return model['data'], 200
 
-
-@app.route("/api/messages", methods=['POST', 'OPTIONS'])
+@app.route("/api/messages", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_create_message():
-    user_sender_handle = 'andrewbrown'
-    user_receiver_handle = request.json['user_receiver_handle']
-    message = request.json['message']
+  user_sender_handle = 'andrewbrown'
+  user_receiver_handle = request.json['user_receiver_handle']
+  message = request.json['message']
 
-    model = CreateMessage.run(message=message, user_sender_handle=user_sender_handle, user_receiver_handle=user_receiver_handle)
-    if model['errors'] is not None:
-        return model['errors'], 422
-    else:
-        return model['data
+  model = CreateMessage.run(message=message,user_sender_handle=user_sender_handle,user_receiver_handle=user_receiver_handle)
+  if model['errors'] is not None:
+    return model['errors'], 422
+  else:
+    return model['data'], 200
+
+@app.route("/api/activities/home", methods=['GET'])
+def data_home():
+  data = HomeActivities.run()
+  return data, 200
+
+@app.route("/api/activities/@<string:handle>", methods=['GET'])
+def data_handle(handle):
+  model = UserActivities.run(handle)
+  if model['errors'] is not None:
+    return model['errors'], 422
+  else:
+    return model['data'], 200
+
+@app.route("/api/activities/search", methods=['GET'])
+def data_search():
+  term = request.args.get('term')
+  model = SearchActivities.run(term)
+  if model['errors'] is not None:
+    return model['errors'], 422
+  else:
+    return model['data'], 200
+
+@app.route("/api/activities", methods=['POST','OPTIONS'])
+@cross_origin()
+def data_activities():
+  user_handle  = 'andrewbrown'
+  message = request.json['message']
+  ttl = request.json['ttl']
+  model = Create
